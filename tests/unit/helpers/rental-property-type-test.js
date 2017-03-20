cb8f375747b1868e00ee8d6f6729bb824240de20
menniti <luiz.otavio.aquino.menniti@gmail.com>
2017-03-20 20:46:12 -0300

    Add helper rental-property-type

---
+++ tests/unit/helpers/rental-property-type-test.js
@@ -0,0 +1,12 @@
+
+import { rentalPropertyType } from 'rent-car-tutorial/helpers/rental-property-type';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | rental property type');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = rentalPropertyType([42]);
+  assert.ok(result);
+});
+
