8198d81bbf7119da7d51a67c2f1c9d807ed6bd80
menniti <luiz.otavio.aquino.menniti@gmail.com>
2017-03-20 20:18:24 -0300

    Add component rental-listing - BugFix

--- tests/integration/components/rental-listing-test.js
+++ tests/integration/components/rental-listing-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('renta-listing', 'Integration | Component | renta listing', {
+moduleForComponent('rental-listing', 'Integration | Component | rental listing', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{renta-listing}}`);
+  this.render(hbs`{{rental-listing}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#renta-listing}}
+    {{#rental-listing}}
       template block text
-    {{/renta-listing}}
+    {{/rental-listing}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
