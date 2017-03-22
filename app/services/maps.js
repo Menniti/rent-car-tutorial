83c560af07a83d06777a0143cee4a5efb8e85e4c
menniti <luiz.otavio.aquino.menniti@gmail.com>
2017-03-22 18:17:00 -0300

    app/service/maps.js - implementing service maps - modify

--- app/services/maps.js
+++ app/services/maps.js
@@ -1,4 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+	init() {
+		if(!this.get('cachedMaps')){
+			this.set('cachedMaps', MapUtil.create());
+		}
+		if(!this.get('MapUtil')){
+			this.set('MapUtil', MapUtil.create());
+		}
+	},
+
+	getElement(location) {
+		let camelizedLocation = location.camelized();
+		let element = this.get(`cachedMaps.${camelizedLocation}`);
+		if(!element) {
+			element = this.createMapElement();
+			this.get('mapUtil').createMap(element, location);
+			this.set(`cachedMaps.${camelizedLocation}`, element);
+		}
+		return element;
+	},
+
+	createMap(){
+		let element = document.createElement('div');
+		element.className = 'map';
+		return element;
+	}   
+	
+
 });
