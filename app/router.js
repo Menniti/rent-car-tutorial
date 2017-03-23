60b9267ad657195aa047e72121323fdc292d7981
menniti <luiz.otavio.aquino.menniti@gmail.com>
2017-03-22 22:43:31 -0300

    app/routes/rentals/index.js / app/templates/rentals/index.hbs / rentals/index / test/unit/routes/rentals/index-test.js - ember generate route rentals/index - create

--- app/router.js
+++ app/router.js
@@ -9,7 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
-  this.route('rentals');
+  this.route('rentals', function() {});
 });
 
 export default Router;
